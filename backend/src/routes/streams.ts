812a2972bdd14cf082d5e7fdc14ffc5c1c7bff34
Morgan Dean <morgandeanhi@gmail.com>
2025-02-09 14:48:50 -0800

    handle single datapoints ax tree

--- backend/src/routes/streams.ts
+++ backend/src/routes/streams.ts
@@ -141,18 +141,21 @@ const processAndCleanup = async (sid: string) => {
         Buffer.from(JSON.stringify(processedResults)),
         10
       );
-      const s3Service = new AWSS3Service(process.env.AWS_ACCESS_KEY, process.env.AWS_SECRET_KEY);
-      console.log('Uploading ax tree to s3...');
-      // wrap this function so the user doesn't have to wait for this
-      (async () => {
-        await s3Service.saveItem({
-          bucket: 'training-gym',
-          file: compressedResults,
-          name: `tree-${sid}.json.zst`
-        });
+      // handle low data culling
+      if (connection.data.length > 1) {
+        const s3Service = new AWSS3Service(process.env.AWS_ACCESS_KEY, process.env.AWS_SECRET_KEY);
+        console.log('Uploading ax tree to s3...');
+        // wrap this function so the user doesn't have to wait for this
+        (async () => {
+          await s3Service.saveItem({
+            bucket: 'training-gym',
+            file: compressedResults,
+            name: `tree-${sid}.json.zst`
+          });
 
-        console.log('done.');
-      })();
+          console.log('done.');
+        })();
+      }
     } catch (error) {
       console.error(`Error processing data for session ${sid}:`, error);
     }
