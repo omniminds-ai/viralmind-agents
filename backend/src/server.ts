6c93966ae4f655373d2e9f9100f47f7d9ebfac8a
Morgan Dean <morgandeanhi@gmail.com>
2025-03-10 17:09:27 -0700

    add header allow

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -46,7 +46,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Expose-Headers', 'auth-token, x-forwarded-for');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type,auth-token,cancelToken,responsetype,x-forwarded-for,x-wallet-address,content-length'
+    'X-Requested-With,content-type,auth-token,cancelToken,responsetype,x-forwarded-for,x-wallet-address,content-length,x-connect-token'
   );
   next();
 });
